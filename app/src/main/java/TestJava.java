7e1a8eb03f3ca102cb3d8bffc524d9626f7c76b0
zhaolonghan <182036137@qq.com>
2015-11-19 09:40:20 +0800

    Rejected commit: Default

---
+++ app/src/main/java/TestJava.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2015/11/19.
+ */
+public class TestJava {
+
+    public static void main(String[] arg){
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
+
+
+
+
+    }
+}
